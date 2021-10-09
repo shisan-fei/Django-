d06732ff5c4c6f4b9f2e1423d7ea749b8d5cb419
shisan-fei <1340673657@qq.com>
2021-10-09 15:39:50 +0800

    模型操作

--- myhome/models.py
+++ myhome/models.py
@@ -7,3 +7,7 @@ class stu(models.Model):
     age = models.IntegerField(default=24)
     sex = models.CharField(max_length=1,default=0)
     address = models.CharField(max_length=50,null=True)
+    
+    def __repr__(self):
+        return f'<stu: stu object (id:{self.id},name:{self.name})>'    
+        #返回<QuerySet [<stu: stu object (id:1,name:wlf)>, <stu: stu object (id:2,name:李四)>]>
--- myhome/views.py
+++ myhome/views.py
@@ -24,7 +24,29 @@ def deam(request):
     # obj.save()     #save是stu继承models类的方法,此时添加完数据，当再次访问deam时，就会将数据添加到数据库myhome_stu
     # print(obj.save())
     #方法2
-    data = {'name':'李四','age':22,'sex':'男','address':'石家庄'}
-    obj = models.stu(**data)
+    # data = {'name':'王五','age':23,'sex':'男','address':'南天门'}
+    # obj = models.stu(**data)
+    # obj.save()
+    
+    #获取数据所有
+    # data = models.stu.objects.all()      #object是stu的方法
+    # print(data)                          #data是对象集，通过对象集可以读取对应数据
+    # for i in data:
+    #     print(i.name)
+        
+    #通过字段获取数据对象
+    # obj=models.stu.objects.get(id=2)   #获取id为2的对象
+    # obj=models.stu.objects.filter(age=23)   #获取age为23的对象，没有或有多个时使用filter就不会报错
+    # print(obj,obj.name)                 #输出对象那个值-->stu object (2) 李四
+    
+    #删除数据
+    # obj = models.stu.objects.get(id=2)
+    # obj.delete()
+     
+    #修改数据
+    obj = models.stu.objects.get(id=3)   #将id为3的name改为老三
+    # obj = models.stu.objects.last() #最后一个
+    obj.name='老三'
     obj.save()
+    
     return HttpResponse('模型测试链接')
